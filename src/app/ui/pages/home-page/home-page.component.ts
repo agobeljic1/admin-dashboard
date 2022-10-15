feb983e4db4390c416c342cc7c8c822958fd2f0c
Adnan Gobeljic <agobeljic1@etf.unsa.ba>
2022-10-15 23:36:41 +0200

    Home page texts

--- src/app/ui/pages/home-page/home-page.component.ts
+++ src/app/ui/pages/home-page/home-page.component.ts
@@ -1,16 +1,23 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { AppState } from 'src/app/store/app.state';
+import { AuthSelectors } from 'src/app/store/auth';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomePageComponent implements OnInit {
+  loggedUsername$!: Observable<string | null>;
 
-  constructor() { }
+  constructor(private readonly store: Store<AppState>) {}
 
   ngOnInit(): void {
+    this.loggedUsername$ = this.store.select(
+      AuthSelectors.selectLoggedUsername
+    );
   }
-
 }
