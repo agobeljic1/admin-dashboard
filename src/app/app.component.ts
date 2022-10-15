a8587ae078c03bae0e9ca9f3d2e905ed0714a566
Adnan Gobeljic <agobeljic1@etf.unsa.ba>
2022-10-16 01:53:03 +0200

    Loading router outlet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
 import { AppState } from './store/app.state';
-import { AuthActions } from './store/auth';
+import { AuthActions, AuthSelectors } from './store/auth';
 
 @Component({
   selector: 'app-root',
@@ -10,10 +11,12 @@ import { AuthActions } from './store/auth';
 })
 export class AppComponent implements OnInit {
   title = 'admin-dashboard';
+  loading$!: Observable<boolean>;
 
   constructor(private store: Store<AppState>) {}
 
   ngOnInit(): void {
+    this.loading$ = this.store.select(AuthSelectors.selectLoadingHeader);
     this.store.dispatch(AuthActions.refreshToken());
   }
 }
